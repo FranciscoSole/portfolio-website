482218576574a56a3056d0da0dcfe9b7834f6c13
Francisco Solé <franciscosoleescande@gmail.com>
2025-01-09 00:13:48 -0300

    added to avoid intro after first load

--- scripts/main.js
+++ scripts/main.js
@@ -1,27 +1,41 @@
-window.onload = loadWelcome();
-
+window.onload = loadWelcome;
 function loadWelcome(){
     try{
-        let aboutMe = document.getElementById("aboutMe")
-        aboutMe.style.display = "none";
-    
-        let vid = document.getElementById("loadingVid");
-        vid.playbackRate = 2;
-        vid.onended = function(){
-            document.getElementById("loading").remove();
-            aboutMe.style.display = "grid";
-            showMainMenu();
+        let aboutMe = document.getElementById("aboutMe");
+        aboutMe.style.display = "none"
+
+        const session = sessionStorage.getItem("loadingVidLoaded")
+        if ((!session) || (session == "false")) {
+            console.log("Loading intro video")
+
+            let vid = document.getElementById("loadingVid");
+            vid.playbackRate = 2;
+            vid.play().then(() => {
+                vid.onended = function () {
+                    sessionStorage.setItem("loadingVidLoaded", "true");
+                    removeLoadingVid(aboutMe)
+                };
+            }).catch((error) => {
+                console.error("Error playing the video: ", error);
+                removeLoadingVid(aboutMe)
+            });
+        } else{
+            console.log("Skipping intro video")
+            removeLoadingVid(aboutMe)
         }
+
+        document.getElementById("mainVid").playbackRate = 0.75;
     } catch(e){
         throw new Error(`[${e}] in main.loadWelcome()`)
     }
 }
 
-function showMainMenu(){
+function removeLoadingVid(aboutMe){
     try{
-        document.getElementById("blind").remove()
-        document.getElementById("mainVid").playbackRate = 0.75;
+        document.getElementById("loading").remove();
+        document.getElementById("blind").remove();
+        aboutMe.style.display = "grid";
     } catch(e){
-        throw new Error(`[${e}] in main.showMainMenu()`)
+        throw new Error(`[${e}] in main.removeLoadingVid()`)
     }
 }
